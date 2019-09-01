e5f15129cce55d6a79ece1903ad5d970620e197c
ryomak <ryooomaaa0413@outlook.com>
2019-09-01 23:28:24 +0900

    fix code

--- line-bot/src/ouin/ouin.go
+++ line-bot/src/ouin/ouin.go
@@ -1,7 +1,7 @@
 package ouin
 
 import (
-	"github.com/ikawaha/kagome/tokenizer"
+ // "github.com/ikawaha/kagome/tokenizer"
 )
 
 type Token struct {
@@ -11,15 +11,20 @@ type Token struct {
 }
 
 func GetHiraganas(sentence string) []Token {
-	list := []Token{}
-	t := tokenizer.New()
-	tokens := t.Tokenize(sentence)
-	for _, token := range tokens {
-		if token.Class == tokenizer.DUMMY {
-			// BOS: Begin Of Sentence, EOS: End Of Sentence.
-			continue
-		}
-		list = append(list, Token{Surface: token.Surface, Type: token.Features()[0], Hiragana: token.Features()[7]})
-	}
+	list := []Token{
+    Token{Surface:"私",Hiragana:"わたし",Type:"主語"},
+    Token{Surface:"は",Hiragana:"は",Type:"助詞"},
+    Token{Surface:"隆",Hiragana:"たかし",Type:"主語"},
+    Token{Surface:"です",Hiragana:"です",Type:"主語"},
+  }
+//	t := tokenizer.New()
+//	tokens := t.Tokenize(sentence)
+//	for _, token := range tokens {
+//		if token.Class == tokenizer.DUMMY {
+//			// BOS: Begin Of Sentence, EOS: End Of Sentence.
+//			continue
+//		}
+//		list = append(list, Token{Surface: token.Surface, Type: token.Features()[0], Hiragana: token.Features()[7]})
+//	}
 	return list
 }
